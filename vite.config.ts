399407520389c73c18ce365667b5fc47fcacdc34
tgvie <166875380+tgvie@users.noreply.github.com>
2025-04-08 00:32:01 +0200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/fed24d-arbetsmetodik-inl-1-tgvie/',
+    base: '/MI-code-smells-practice/',
     root: './',
     build: {
         outDir: 'dist',
